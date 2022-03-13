fc79f14e1bdefcc436cfa107653f120c880126fc
KARAN SINGH CHOUHAN <karanch3001@gmail.com>
2022-03-13 22:36:07 +0530

    Node childProcess and os module basics

--- node/childProcess.js
+++ node/childProcess.js
@@ -1,5 +1,16 @@
 let cp = require("child_process");
-//console.log(cp);
+console.log(cp);
 
 //For Windows Calculaator
-cp.execFileSync("calc");
+//cp.execSync("calc");
+//cp.execSync("code");
+let content = cp.execSync("node test.js");
+
+//Printing out content from test.js file
+//console.log("Output of test.js file:- "+ content);
+
+// This will give us values in Binary form
+//console.log(content);
+
+// To convert it into our original form
+console.log(""+ content);
--- node/os.js
+++ node/os.js
@@ -1,4 +1,7 @@
 let os = require("os");
-console.log(os);
+//console.log(os);
 console.log(os.hostname());
+console.log(os.arch());
+console.log(os.hostname());
+console.log(os.networkInterfaces());
 console.log(os.userInfo());
