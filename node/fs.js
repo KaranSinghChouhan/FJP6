5d8f01c5f5274d700106a132af84170c3f20f393
KARAN SINGH CHOUHAN <karanch3001@gmail.com>
2022-03-14 22:29:52 +0530

    CRUD Operation commands completed

--- node/fs.js
+++ node/fs.js
@@ -4,10 +4,28 @@ let fs = require("fs");  //We use this to perform CRUD actions (Crete-Read-Updat
 
 let path = require("path");
 let filePath = path.join(__dirname,"file.txt");
-console.log("filePath");
+//console.log("filePath");
 
+//1.Creating/Writing
 //It creates file if it does not exist else override
-fs.writeFileSync(filePath,"Hello I am a text file");
-fs.writeFileSync(filePath,"Writing the content in file again");
+fs.writeFileSync(filePath,"Hello I am a text file.");
+//fs.writeFileSync(filePath,"Again writing in existing file");
+
+//2. Reading
+console.log("Before Update: ")
+let content = fs.readFileSync(filePath,'utf-8');
+console.log(content);
+
+//3. Updating
+ fs.appendFileSync(filePath,"\nNew content added");
+ console.log("Before Update: ")
+ console.log(fs.readFileSync(filePath,'utf-8'))
+
+
+ //4. Deleting
+ fs.unlinkSync(filePath);
+
+
+
 
 
