a62355c2e1494aae2e30c66657e3b237b1cda3e5
KARAN SINGH CHOUHAN <karanch3001@gmail.com>
2022-03-14 22:53:46 +0530

    Calculator module - exporting our functions

---
+++ node/module/testModule.js
@@ -0,0 +1,7 @@
+let calc = require("./calculator.js");
+console.log(calc);
+
+console.log(calc.addition(4,8));
+console.log(calc.substraction(4,8));
+console.log(calc.division(4,8));
+console.log(calc.multiplication(4,8));
