2df0e31d2ff4bd3e2666766987955f1fa29c75cf
Karan Singh <karanch3001@gmail.com>
2022-12-19 22:55:23 +0530

    child_process

--- Module2/node/childProcess.js
+++ Module2/node/childProcess.js
@@ -2,7 +2,12 @@ let cp = require('child_process');
 //console.log(cp);
 
 //Open calculator with the help of this file
-//cp.execFileSync('calc');
+//cp.execSync('calc');
+
+//Opening VSCode
+//cp.execSync('code');
 
 //opening test.js file from this file
-console.log("Let's run test file : ", cp.execFileSync('test.js'));
+//console.log("Let's run test file : ", cp.execFileSync('test.js'));
+let content = cp.execSync("node test.js");
+console.log("" + content);//empty string for convert it into string from binary
