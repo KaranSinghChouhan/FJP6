f4f7523a7395040e8a85bdda4e17ad2bc017f8a2
Karan Singh <karanch3001@gmail.com>
2022-12-21 07:33:20 +0530

    node path

---
+++ Module2/node/path.js
@@ -0,0 +1,25 @@
+//"E:\2. Code\Pepcoding\FJP6\Module2\node\path.js"
+
+const { dirname } = require("path");
+let path = require("path");
+//console.log(path);
+
+//To know the  exyension of file
+let extensionName = path.extname("E:\2. Code\Pepcoding\FJP6\Module2\node\path.js");
+//console.log(extensionName);
+
+//To know the full name + extension name of file
+// let baseName = path.basename(__filename);
+// console.log(baseName);
+
+//to know the path till file
+//console.log(__filename);
+
+//to know the path till folder in which our file is
+let dirPath = __dirname;
+console.log(dirPath);
+
+//joining file name in our file path
+let newFilePath = path.join(dirPath,"test.js");
+console.log(newFilePath);
+
