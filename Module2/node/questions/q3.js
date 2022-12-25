472eed1c778e024004bed1f747bd2ff8d6f04423
Karan Singh <karanch3001@gmail.com>
2022-12-26 01:26:18 +0530

    q4 solved

--- Module2/node/questions/q3.js
+++ Module2/node/questions/q3.js
@@ -3,11 +3,11 @@
 let fs = require("fs");
 let path = require("path");
 
-let htmlPath = path.join(__dirname,"..","..","html")
+let htmlPath = path.join(__dirname, "..", "..", "html")
 if (!fs.existsSync(htmlPath)) {
     fs.mkdirSync(htmlPath);
 }
 
-let sourcePath = path.join(__dirname,"..","..","..","Module1","firstLecture","index.html");
-let destnationPath = path.join(__dirname,"..","..","html","index.html");
+let sourcePath = path.join(__dirname, "..", "..", "..", "Module1", "firstLecture", "index.html");
+let destnationPath = path.join(__dirname, "..", "..", "html", "index.html");
 fs.copyFileSync(sourcePath, destnationPath);
--- Module2/node/questions/q4.js
+++ Module2/node/questions/q4.js
@@ -3,3 +3,13 @@
 let fs = require("fs");
 let path = require("path");
 
+let folderPath = path.join(__dirname,"..","unorganised");
+
+let content = fs.readdirSync(folderPath);
+let extArr = [];
+
+for(let i = 0; i < content.length; i++){
+    let extensionName = path.extname(content[i]);
+    extArr.push(extensionName);
+}
+console.log(extArr);
