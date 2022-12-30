a368d6d06e580b5c9a5dacd8822d2a8eeec7e47b
Karan Singh <karanch3001@gmail.com>
2022-12-30 23:42:24 +0530

    request_actvity_callback

--- Module2/web_scrapping/request.js
+++ Module2/web_scrapping/request.js
@@ -1,8 +1,19 @@
 const request = require('request');
-request('http://www.google.com', function (error, response, body) {
+
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+const link = "https://www.espncricinfo.com/series/ipl-2021-1249214/chennai-super-kings-vs-kolkata-knight-riders-final-1254117/full-scorecard";
+
+request(link, cb);//this line means: call this function after below work is comleted
+
+function cb(error, response, html) {
     if (error) {
         console.error('error:', error); // Print the error if one occurred
     } else {
-        console.log('body:', body); // Print the HTML for the Google homepage.
+        const dom = new JSDOM(html);
+        const document = dom.window.document;
+        let teamsName = document.querySelectorAll(".ds-text-tight-l.ds-font-bold.ds-text-ui-typo.hover:ds-text-ui-typo-primary.ds-block.ds-truncate");
+        console.log(teamsName[1].textContent);
     }
-});
+}
