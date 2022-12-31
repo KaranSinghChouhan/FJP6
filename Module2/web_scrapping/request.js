b97511d770eaa7906aeef7ea2c968f69e53e8b61
Karan Singh <karanch3001@gmail.com>
2022-12-31 23:14:46 +0530

    highestWicketTaker done

--- Module2/web_scrapping/request.js
+++ Module2/web_scrapping/request.js
@@ -1,19 +1,11 @@
 const request = require('request');
 
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+request('http://www.google.com', cb);
 
-const link = "https://www.espncricinfo.com/series/ipl-2021-1249214/chennai-super-kings-vs-kolkata-knight-riders-final-1254117/full-scorecard";
-
-request(link, cb);//this line means: call this function after below work is comleted
-
-function cb(error, response, html) {
+function cb(error, response, body) {
     if (error) {
-        console.error('error:', error); // Print the error if one occurred
+        console.log('error: ', error);//prints the error if one occured
     } else {
-        const dom = new JSDOM(html);
-        const document = dom.window.document;
-        let teamsName = document.querySelectorAll(".ds-text-tight-l.ds-font-bold.ds-text-ui-typo.hover:ds-text-ui-typo-primary.ds-block.ds-truncate");
-        console.log(teamsName[1].textContent);
+        console.log('body: ', body);//prints the html for the google homepage
     }
 }
