ee6008a0ac57480486c748f419bad466365df059
Karan Singh <karanch3001@gmail.com>
2023-01-06 23:07:48 +0530

    google opened via automation

--- Module2/automation/raw.js
+++ Module2/automation/raw.js
@@ -14,4 +14,11 @@ let browserPromise = puppeteer.launch({ headless: false });
 browserPromise.then(function (browserInstance) {
     console.log("Browser is opened");
     let pagePromise = browserInstance.newPage();
+    return pagePromise;
+}).then(function(page){
+    console.log("page is opened");
+    let urlPromise = page.goto("https://www.google.com/");
+    return urlPromise;
+}).then(function(){
+    console.log("Google is opened");
 })
